9e5265014cdc41e8f4e7bd5c1cd8fe3a48a19f3a
Josiah <josiah.pilon@yahoo.ca>
2021-04-17 14:13:32 -0700

    Custom hook that changes the mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function useVisualMode() {
-  
+export default function useVisualMode(initial) {
+  const [ mode, setMode ] = useState(initial);
+  const [ history, setHistory ] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    let newHistory = [ ...history, newMode ];
+
+    if(replace) {
+      
+      const replacement = newHistory.slice(-1);      
+      newHistory = [history[0], ...replacement];
+      newMode = newHistory[newHistory.length - 1];
+    }
+
+    setMode(newMode);
+    setHistory(newHistory);
+    return;
+  }
+
+  const back = () => {
+
+    const backHistory = history.slice(0, -1);
+    const lastMode = backHistory[backHistory.length - 1]
+
+    setMode(lastMode);
+    setHistory(backHistory);
+    return;
+  }
+
+  return { mode, transition, back };
 }
+
