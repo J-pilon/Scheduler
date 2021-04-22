eaf106ad2215748871423ff2166076e65009bfb7
Josiah <josiah.pilon@yahoo.ca>
2021-04-21 19:28:38 -0700

    Refactored application test to use async await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,16 +6,16 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-describe("API requests", () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Application", () => {
 
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });          
-  
-});
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Monday"));
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
   
 })
