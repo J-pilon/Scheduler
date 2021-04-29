ac11ea7fefafc9631f19f611db8755c19da7aee5
Josiah <josiah.pilon@yahoo.ca>
2021-04-29 16:43:12 -0700

    DRYed up my code by creating a function to get the current day from state. This function is then called in getAppointmentsForDay and getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,18 @@
-// returns an array of appointment objects for the current day
-function getAppointmentsForDay(state, day) {
- 
-  const days = state.days;
+function getCurrentDay(days) {
+
+  // const days = state.days;
   const currentDay = days.find(item => item.name === day);
   if (!currentDay || state.days.length === 0) {
     return [];
   }
+  return currentDay;
+}
+
+// returns an array of appointment objects for the current day
+function getAppointmentsForDay(state, day) {
+ 
+  const currentDay = getCurrentDay(state.days);
+  
   const appointmentIds = currentDay.appointments;
   const appointments = appointmentIds.map(id => state.appointments[id]);
 
@@ -30,11 +37,13 @@ return updatedInterview;
 // returns an array of objects that contain the interviewers id, name and avatar for that day
 function getInterviewersForDay(state, day) {
  
-  const days = state.days;
-  const currentDay = days.find(item => item.name === day);
-  if (!currentDay || state.days.length === 0) {
-    return [];
-  }
+  // const days = state.days;
+  // const currentDay = days.find(item => item.name === day);
+  // if (!currentDay || state.days.length === 0) {
+  //   return [];
+  // }
+
+  const currentDay = getCurrentDay(state.days);
 
   const interviewersForDayId = currentDay.interviewers;
   const interviewersForDay = interviewersForDayId.map(id => state.interviewers[id]);
